cf502223ba7b3b4e0257c705e7ce6de61590033d
Neale Taylor <nealeptaylor@gmail.com>
2022-04-20 16:33:07 -0400

    add solution for jaden casing

--- jadenCasingStrings.js
+++ jadenCasingStrings.js
@@ -1,3 +1,13 @@
 const jadenCase = function(string) {
-  
+  let array = string.split(' ');
+  // console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    let word = array[i];
+    word = word.charAt(0).toUpperCase() + word.slice(1);
+    array.splice(i, 1, word);
+  }
+  let newString = array.join(' ');
+  return newString;
 };
+
+console.log(jadenCase("How can mirrors be real if our eyes aren't real"));
