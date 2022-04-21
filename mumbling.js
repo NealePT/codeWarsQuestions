49abdb674e8bd7d9374f341c5aa4b4b8dbbacdaa
Neale Taylor <nealeptaylor@gmail.com>
2022-04-21 15:14:36 -0400

    implement solution

--- mumbling.js
+++ mumbling.js
@@ -8,5 +8,15 @@
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 const accum = (string) => {
-
+  let array = string.split('');
+  for (let i = 0; i < array.length; i++) {
+    let word = array[i];
+    word = word.toLowerCase().repeat(i + 1);
+    word = word.charAt(0).toUpperCase() + word.slice(1);
+    array.splice(i, 1, word);
+  }
+  
+  return array.join('-');
 };
+
+console.log(accum("abcd"));
