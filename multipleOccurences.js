1d24685a86a39fd8a154795bfcc934246a507a27
Neale Taylor <nealeptaylor@gmail.com>
2022-04-21 17:15:27 -0400

    implement solution

--- multipleOccurences.js
+++ multipleOccurences.js
@@ -4,6 +4,12 @@
 // Given a list and a number, create a new list that contains each number of lst at most N times without reordering. For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3]. With list [20,37,20,21] and number 1, the result would be [20,37,21].
 
 
-const deleteNth = (arr, n) => {
-  
+const deleteNth = (array, n) => {
+  let cache = {};
+  return array.filter(function(x) {
+    cache[x] = (cache[x] || 0) + 1;
+    return cache[x] <= n;
+  });
 };
+
+console.log(deleteNth([20,37,20,21], 1)); // [20,37,21]
