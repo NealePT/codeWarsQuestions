76d0b287b3727ceac6af3120159ae8b6e299bce4
Neale Taylor <nealeptaylor@gmail.com>
2022-04-21 17:40:11 -0400

    implement solution

--- moveZerosToTheEnd.js
+++ moveZerosToTheEnd.js
@@ -3,5 +3,17 @@
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
 const moveZeros = (array) => {
-  
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== 0) {
+      array[count++] = array[i];
+      console.log(count);
+    }
+  }
+  for (let i = count; i < array.length; i++) {
+    array[i] = 0;
+  }
+  return array;
 };
+
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])); // [false,1,1,2,1,3,"a",0,0]
