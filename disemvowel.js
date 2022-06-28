a7b1003e664c21d3548f4a120c181cad138387e0
Neale Taylor <nealeptaylor@gmail.com>
2022-06-28 11:06:01 -0400

    add tests

--- disemvowel.js
+++ disemvowel.js
@@ -12,3 +12,6 @@
 const disemvowel = (string) => {
 
 };
+
+console.log(disemvowel("This website is for losers LOL!")); // Ths wbst s fr lsrs LL!
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read")); // N ffns bt,\nYr wrtng s mng th wrst 'v vr rd
