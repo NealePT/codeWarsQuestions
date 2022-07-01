e90a91f968125e4a64accf2335f8adb80738cce4
Neale Taylor <nealeptaylor@gmail.com>
2022-07-01 11:15:54 -0400

    add tests

--- basicMathOperation.js
+++ basicMathOperation.js
@@ -10,5 +10,10 @@
 // ('/', 49, 7) --> 7
 
 const basicOp = (operation, value1, value2) => {
-
+  
 };
+
+console.log(basicOp('+', 4, 7)); // 11
+console.log(basicOp('-', 15, 18)); // -3
+console.log(basicOp('*', 5, 5)); // 25
+console.log(basicOp('/', 49, 7)); // 7
