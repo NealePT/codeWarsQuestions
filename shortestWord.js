002ff8865090e74ac7614d4b9b9fc825b107c087
Neale Taylor <nealeptaylor@gmail.com>
2022-07-05 14:26:49 -0400

    implement solution

--- shortestWord.js
+++ shortestWord.js
@@ -3,7 +3,14 @@
 // String will never be empty and you do not need to account for different data types.
 
 const findShort = (str) => {
-
+  let array = str.split(' ');
+  let result = 0;
+  for (let word of array) {
+    if (word.length < result || result === 0) {
+      result = word.length;
+    }
+  }
+  return result;
 };
 
 console.log(findShort('turns out random test cases are easier than writing out basic ones')); // 3
