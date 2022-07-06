501db556a34e6541e546ab5265cbc39812fcac6d
Neale Taylor <nealeptaylor@gmail.com>
2022-07-06 16:27:10 -0400

    implement solution

--- populationGrowth.js
+++ populationGrowth.js
@@ -1,9 +1,9 @@
 // In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
 
-// At the end of the first year there will be: 
+// At the end of the first year there will be:
 // 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
 
-// At the end of the 2nd year there will be: 
+// At the end of the 2nd year there will be:
 // 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
 
 // At the end of the 3rd year there will be:
@@ -28,8 +28,12 @@
 // Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
 
 
-const nbYear = (p0, percent, aug, p) {
-
+const nbYear = (p0, percent, aug, p) => {
+  let years = 0;
+  for (years; p0 < p; years++) {
+    p0 += Math.floor(p0 * (percent / 100) + aug);
+  }
+  return years;
 };
 
 console.log(nbYear(1500, 5, 100, 5000)); // 15
