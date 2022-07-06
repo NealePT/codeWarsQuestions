ae230c9d0b6c9db269f03847d914c07660607b4f
Neale Taylor <nealeptaylor@gmail.com>
2022-07-06 16:39:26 -0400

    add tests

--- sumOfFirstNthTerm.js
+++ sumOfFirstNthTerm.js
@@ -17,5 +17,9 @@
 // 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
 
 const SeriesSum = (n) => {
-  
+
 };
+
+console.log(SeriesSum(1)); // 1.00
+console.log(SeriesSum(2)); // 1.25
+console.log(SeriesSum(5)); // 1.57
