d6868cac6350ed935d82959f10628a1aebdefd71
Neale Taylor <nealeptaylor@gmail.com>
2022-07-06 16:36:14 -0400

    implement solution

--- calculateAverage.js
+++ calculateAverage.js
@@ -3,7 +3,12 @@
 // Note: Empty arrays should return 0.
 
 const find_average = (array) => {
-
+  let result = 0;
+  if (array.length === 0) return 0;
+  for (let i = 0; i < array.length; i++) {
+    result = result + array[i];
+  }
+  return result / array.length;
 };
 
 console.log(find_average([1,1,1])); // 1
