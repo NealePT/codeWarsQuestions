18096e2cc58729cc1985a6721c55d31f68dd8486
Neale Taylor <nealeptaylor@gmail.com>
2022-07-07 14:54:14 -0400

    implement solution

--- stringRepeat.js
+++ stringRepeat.js
@@ -5,7 +5,11 @@
 // 5, "Hello" -> "HelloHelloHelloHelloHello"
 
 const repeatStr = (n, s) => {
-
+  let array = [];
+  for (let i = 0; i < n; i++) {
+    array.push(s);
+  }
+  return array.join("");
 };
 
 console.log(repeatStr(6, "I")); // "IIIIII"
