a0f95d8c54dd7997f4345cc904ed8bbc1d7bbcd4
Neale Taylor <nealeptaylor@gmail.com>
2022-07-11 16:59:18 -0400

    add tests

--- sumOfPositive.js
+++ sumOfPositive.js
@@ -5,5 +5,9 @@
 // Note: if there is nothing to sum, the sum is default to 0.
 
 const positiveSum = (arr) => {
-  
+
 };
+
+console.log(positiveSum([1, 2, 3, 4, 5])); // 15
+console.log(positiveSum([1, -2, 3, 4, 5])); // 13
+console.log(positiveSum([-1, -2, -3, -4, -5])); // 13
