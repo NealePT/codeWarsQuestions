8d9548872b550c4997db77b3040f910f339e0faf
Neale Taylor <nealeptaylor@gmail.com>
2022-07-11 16:55:33 -0400

    implement solution

--- replaceWithAlphabetPosition.js
+++ replaceWithAlphabetPosition.js
@@ -10,8 +10,18 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
 const alphabetPosition = (text) => {
-
+  text.split(' ').join('');
+  let chari = "";
+  let arr = [];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+  for (let i = 0; i < text.length; i++) {
+    chari = text.charAt(i).toLowerCase();
+    if (alphabet.indexOf(chari) > -1) {
+      arr.push(alphabet.indexOf(chari) + 1);
+    }
+  }
+  return arr.join(' ');
 };
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
-console.log(alphabetPosition("The narwhal bacons at midnight.")); // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+// console.log(alphabetPosition("The narwhal bacons at midnight.")); // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
