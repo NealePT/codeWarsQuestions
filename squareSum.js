c27364a9c7d5fdd0ac95c00be1b8d7ec412801f0
Neale Taylor <nealeptaylor@gmail.com>
2022-07-11 14:29:21 -0400

    add tests

--- squareSum.js
+++ squareSum.js
@@ -2,6 +2,9 @@
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-const squareSum = (numbers) => {
+const squareSum = (array) => {
 
 };
+
+console.log(squareSum([1, 2, 2])); // 9
+console.log(squareSum([0, 3, 4, 5])); // 50
