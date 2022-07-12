84d9f130c2a59444c286daec771599ee66515c82
Neale Taylor <nealeptaylor@gmail.com>
2022-07-12 13:56:33 -0400

    implement solution

--- gradeBook.js
+++ gradeBook.js
@@ -9,7 +9,12 @@
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 
 const getGrade = (s1, s2, s3) => {
-
+  let avg = (s1 + s2 + s3) / 3;
+  if (avg < 60)  return "F";
+  else if (avg < 70) return "D";
+  else if (avg < 80) return "C";
+  else if (avg < 90) return "B";
+  else return "A";
 };
 
 console.log(getGrade(100,85,96)); // 'A'
