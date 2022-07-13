3d15e9e27178ad4db45fa2c19ed98b6d99885c2c
Neale Taylor <nealeptaylor@gmail.com>
2022-07-13 13:02:40 -0400

    add tests

--- countPosSumNeg.js
+++ countPosSumNeg.js
@@ -10,3 +10,6 @@
 const countPositivesSumNegatives = (input) => {
 
 };
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])); // [10, -65]
+console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])); // [8, -50]
