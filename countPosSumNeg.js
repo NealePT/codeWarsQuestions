7c6aa4cda5bd9b302468415e68aa5bfe6c23052a
Neale Taylor <nealeptaylor@gmail.com>
2022-07-13 13:12:30 -0400

    implement solution

--- countPosSumNeg.js
+++ countPosSumNeg.js
@@ -8,8 +8,21 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 const countPositivesSumNegatives = (input) => {
-
+  let posCount = 0;
+  let negSum = 0;
+  if (input.length < 1 || input === null)  {
+    return [];
+  }
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      posCount++;
+    } else if (input[i] < 0) {
+      negSum += input[i];
+    }
+  }
+  return [posCount, negSum];
 };
 
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])); // [10, -65]
 console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])); // [8, -50]
+console.log(countPositivesSumNegatives([]));
