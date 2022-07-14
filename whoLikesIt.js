08adb0049b9ab27674d9c8647051d6b0650619d8
Neale Taylor <nealeptaylor@gmail.com>
2022-07-14 15:50:58 -0400

    add tests

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -13,3 +13,9 @@
 const likes = (names) => {
 
 };
+
+console.log(likes([])); // "no one likes this"
+console.log(likes(["Peter"])); // "Peter likes this"
+console.log(likes(["Jacob", "Alex"])); // "Jacob and Alex like this"
+console.log(likes(["Max", "John", "Mark"])); // "Max, John and Mark like this"
+console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // "Alex, Jacob, and 2 others like this"
