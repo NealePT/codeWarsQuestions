40db7f27113f7ed4126106d1a63dffe4fae1233d
Neale Taylor <nealeptaylor@gmail.com>
2022-08-02 12:33:44 -0400

    implement solution

--- testing123.js
+++ testing123.js
@@ -10,8 +10,12 @@
 // ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
 
 const number = (array) => {
-
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(`${i + 1}: ${array[i]}`);
+  }
+  return result;
 };
 
-console.log(number([])); // []
+// console.log(number([])); // []
 console.log(number(["a", "b", "c"])); // ["1: a", "2: b", "3: c"]
