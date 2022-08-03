1cd3a67a756ec9dc2f4f0409537789db3ec5e74d
Neale Taylor <nealeptaylor@gmail.com>
2022-08-03 12:28:00 -0400

    implement solution

--- sumWIthoutHighAndLow.js
+++ sumWIthoutHighAndLow.js
@@ -12,8 +12,30 @@
 
 // If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 
-const sumArray = (array) => {
+// const sumArray = (array) => {
+//   let sum = 0;
+//   let min = Math.min(...array);
+//   let max = Math.max(...array);
+//   for (let num of array) {
+//     if (num !== min && num !== max) {
+//       sum += num;
+//     }
+//   }
+//   return sum;
+// };
 
+const sumArray = (array) => {
+  if (array === null || array === undefined || array === []) {
+    return 0;
+  }
+  array = array.sort(function(a,b) {
+    return a - b;
+  });
+  let total = 0;
+  for (let i = 1; i < array.length - 1; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
 console.log(sumArray([6, 2, 1, 8, 10])); // 16
