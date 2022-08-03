2ba674504d416ee65746b91f0e2d5215f0ca166a
Neale Taylor <nealeptaylor@gmail.com>
2022-08-03 11:09:23 -0400

    add tests

--- highestAndLowest.js
+++ highestAndLowest.js
@@ -14,3 +14,7 @@
 const highAndLow = (numbers) => {
 
 };
+
+console.log(highAndLow(1 2 3 4 5)); // "5 1"
+console.log(highAndLow(1 2 -3 4 5)); // "5 -3"
+console.log(highAndLow(1 9 3 4 -5)); // "9 -5"
