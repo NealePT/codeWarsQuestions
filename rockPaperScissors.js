0651c2a0cf44f9ef60dde660b6e554cbd5a79d6d
Neale Taylor <nealeptaylor@gmail.com>
2022-08-08 13:02:43 -0400

    implement solution

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,9 +9,33 @@
 // "paper", "paper" --> "Draw!"
 
 const rps = (p1, p2) => {
-
+  if (p1 === "rock") {
+    if (p2 === "rock") {
+      return "Draw!";
+    } else if (p2 === "paper") {
+      return "Player 2 won!";
+    } else if (p2 === "scissors") {
+      return "Player 1 won!";
+    }
+  } else if (p1 === "paper") {
+    if (p2 === "paper") {
+      return "Draw!";
+    } else if (p2 === "scissors") {
+      return "Player 2 won!";
+    } else if (p2 === "rock") {
+      return "Player 1 won!";
+    }
+  } else if (p1 === "scissors") {
+    if (p2 === "scissors") {
+      return "Draw!";
+    } else if (p2 === "rock") {
+      return "Player 2 won!";
+    } else if (p2 === "paper") {
+      return "Player 1 won!";
+    }
+  }
 };
 
 console.log(rps("scissors", "rock")); // "Player 2 won!"
 console.log(rps("rock", "rock")); // "Draw!"
-console.log(rps("scissors", "peper")); // "Player 1 won!"
+console.log(rps("scissors", "paper")); // "Player 1 won!"
