33532c05bb0dd0389869b919471e6e777fc28d75
Neale Taylor <nealeptaylor@gmail.com>
2022-08-09 10:53:44 -0400

    implement solution

--- highestScoringWord.js
+++ highestScoringWord.js
@@ -8,8 +8,21 @@
 
 // All letters will be lowercase and all inputs will be valid.
 
-const high = (string) => {
-
+const high = (x) => {
+  const words = x.split(' ');
+  const alphabetMap = {};
+  for (let i = 'a'.charCodeAt(), j = 1; i <= 'z'.charCodeAt(); i++, j++) {
+    alphabetMap[i] = j;
+  }
+  let highestScoringWord = { word: '', score: 0 };
+  words.forEach(w => {
+    const chars = w.split('');
+    const sumOfChars = chars.reduce((count, char) => count + alphabetMap[char.charCodeAt()], 0);
+    if (sumOfChars > highestScoringWord.score) {
+      highestScoringWord = { word: w, score: sumOfChars };
+    }
+  });
+  return highestScoringWord.word;
 };
 
 console.log(high("man i need a taxi up to ubud")); // "taxi"
