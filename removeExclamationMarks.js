84d32eb90b48dbe9caaa9c314182c8ce6572b101
Neale Taylor <nealeptaylor@gmail.com>
2022-08-11 10:26:49 -0400

    add test

--- removeExclamationMarks.js
+++ removeExclamationMarks.js
@@ -1,5 +1,7 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 const removeExclamationMarks = (string) => {
-  
+
 };
+
+console.log(removeExclamationMarks("Hello World!")); // Hello World
