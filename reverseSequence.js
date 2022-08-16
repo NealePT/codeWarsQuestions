6aaaee9872ed1383527c510c7794629e5d5ce66c
Neale Taylor <nealeptaylor@gmail.com>
2022-08-16 12:28:23 -0400

    implement solution

--- reverseSequence.js
+++ reverseSequence.js
@@ -3,7 +3,11 @@
 // Example : n=5 --> [5,4,3,2,1]
 
 const reverseSeq = (n) => {
-
+  let result = [];
+  for (let i = n; i > 0; i--) {
+    result.push(i);
+  }
+  return result;
 };
 
 console.log(reverseSeq(5)); // [5,4,3,2,1]
