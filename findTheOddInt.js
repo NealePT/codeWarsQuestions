93918b00715bc14e1b24145cd566e2d9b3c97b51
Neale Taylor <nealeptaylor@gmail.com>
2022-08-22 13:32:03 -0400

    add tests

--- findTheOddInt.js
+++ findTheOddInt.js
@@ -12,3 +12,8 @@
 const findOdd = (array) => {
 
 };
+
+console.log(findOdd([7])); // 7;
+console.log(findOdd([1,1,2])); // 2;
+console.log(findOdd([0,1,0,1,0])); // 0;
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1])); //4;
