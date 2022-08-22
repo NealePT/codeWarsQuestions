4926454d85ca44d342ed948e9f3711b6216db946
Neale Taylor <nealeptaylor@gmail.com>
2022-08-22 16:49:21 -0400

    add tests

--- numberInExpandedForm.js
+++ numberInExpandedForm.js
@@ -11,3 +11,7 @@
 const expandedForm = (num) => {
 
 };
+
+console.log(expandedForm(12)); // "10 + 2"
+console.log(expandedForm(42)); // "40 + 2"
+console.log(expandedForm(70304)); // "70000 + 300 + 4"
