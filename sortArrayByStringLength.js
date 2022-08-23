b4bbaf723ec9730beb6013ff750c89b3cfe9707d
Neale Taylor <nealeptaylor@gmail.com>
2022-08-23 11:51:05 -0400

    implement solution

--- sortArrayByStringLength.js
+++ sortArrayByStringLength.js
@@ -11,7 +11,7 @@
 // All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 
 const sortByLength = (array) => {
-
+  return array.sort((a, b) => a.length - b.length);
 };
 
 console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"])); // ["Eyes", "Glasses", "Monocles", "Telescopes"]
