890b757492cd664e223ece5c452daeb524ba3b95
Neale Taylor <nealeptaylor@gmail.com>
2022-08-26 14:14:47 -0400

    add tests

--- countDivisorsOfNumber.js
+++ countDivisorsOfNumber.js
@@ -11,3 +11,8 @@
 const getDivisorsCnt = (num) => {
 
 };
+
+console.log(getDivisorsCnt(4)); // 3
+console.log(getDivisorsCnt(5)); // 2
+console.log(getDivisorsCnt(12)); // 6
+console.log(getDivisorsCnt(30)); // 8
