dbf0b009e6c50dcc35276379855180a336ca7f00
Neale Taylor <nealeptaylor@gmail.com>
2022-08-29 11:09:20 -0400

    add tests

--- squareOfSquares.js
+++ squareOfSquares.js
@@ -23,3 +23,10 @@
 const isSquare = (n) => {
 
 };
+
+console.log(isSquare(-1)); // false
+console.log(isSquare(0)); // true
+console.log(isSquare(3)); // false
+console.log(isSquare(4)); // true
+console.log(isSquare(25)); // true
+console.log(isSquare(26)); // false
