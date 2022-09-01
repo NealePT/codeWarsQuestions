878452716dad1da2b3f1fdde97d64dd8175ac01d
Neale Taylor <nealeptaylor@gmail.com>
2022-09-01 11:21:09 -0400

    uncomment

--- squareOfSquares.js
+++ squareOfSquares.js
@@ -20,13 +20,13 @@
 // 26  =>  false
 
 
-// const isSquare = (n) => {
-//   let squareRoot = Math.sqrt(n);
-//   if (squareRoot % 1 === 0) {
-//     return true;
-//   }
-//   return false;
-// };
+const isSquare = (n) => {
+  let squareRoot = Math.sqrt(n);
+  if (squareRoot % 1 === 0) {
+    return true;
+  }
+  return false;
+};
 
 // const isSquare = (n) => {
 //   return Number.isInteger(Math.sqrt(n));
