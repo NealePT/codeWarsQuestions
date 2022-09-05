16bf37a301f0f2b254c786e156f00fb7e6c3a100
Neale Taylor <nealeptaylor@gmail.com>
2022-09-05 15:01:11 -0400

    implement solution

--- sortNumbers.js
+++ sortNumbers.js
@@ -6,7 +6,10 @@
 // solution(null); // should return []
 
 const solution = (nums) => {
-
+  if (nums === null) {
+    return [];
+  }
+  return nums.sort((a, b) => a - b);
 };
 
 console.log(solution([1, 2, 10, 50, 5])); // [1,2,5,10,50]
