5461228fae821030ec92fecaa0e031082473aa46
Neale Taylor <nealeptaylor@gmail.com>
2022-09-05 15:02:15 -0400

    add one line solution

--- sortNumbers.js
+++ sortNumbers.js
@@ -5,11 +5,15 @@
 // solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 // solution(null); // should return []
 
+// const solution = (nums) => {
+//   if (nums === null) {
+//     return [];
+//   }
+//   return nums.sort((a, b) => a - b);
+// };
+
 const solution = (nums) => {
-  if (nums === null) {
-    return [];
-  }
-  return nums.sort((a, b) => a - b);
+  return nums !== null ? nums.sort(function(a,b) { return a - b }) : [];
 };
 
 console.log(solution([1, 2, 10, 50, 5])); // [1,2,5,10,50]
