d5163e2fb0fdf170b366d85eef8c381416f89c40
Neale Taylor <nealeptaylor@gmail.com>
2022-09-06 10:49:52 -0400

    add tests

--- makeNegative.js
+++ makeNegative.js
@@ -12,3 +12,8 @@
 const makeNegative = (num) => {
 
 };
+
+console.log(makeNegative(1)); // -1
+console.log(makeNegative(5)); // -5
+console.log(makeNegative(0)); // 0
+console.log(makeNegative(0.12)); // -0.12
