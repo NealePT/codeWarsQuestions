5b56e9f86b7f56565cf1c9207c9444163c6c830e
Neale Taylor <nealeptaylor@gmail.com>
2022-09-07 15:00:15 -0400

    spacing

--- sumOfSequence.js
+++ sumOfSequence.js
@@ -19,6 +19,7 @@ const sequenceSum = (begin, end, step) => {
   return sum;
 };
 
+
 console.log(sequenceSum(2, 2, 2)); // 2
 console.log(sequenceSum(2, 6, 2)); // 12
 console.log(sequenceSum(1, 5, 1)); // 15
