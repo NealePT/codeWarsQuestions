07020c2391d9e52a44307242378eb810c9c01787
Neale Taylor <nealeptaylor@gmail.com>
2022-09-13 09:59:47 -0400

    implement alternate solution

--- findTheStray.js
+++ findTheStray.js
@@ -8,19 +8,27 @@
 // [1, 1, 2] ==> 2
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
 
+// const stray = (numbers) => {
+//   let arr1 = [];
+//   let arr2 = [];
+//   for (let number of numbers) {
+//     if (arr1.length === 0) {
+//       arr1.push(number);
+//     } else if (arr1[0] === number) {
+//       arr1.push(number);
+//     } else {
+//       arr2.push(number);
+//     }
+//   }
+//   return (arr1.length === 1 ? Number(arr1) : Number(arr2));
+// };
+
 const stray = (numbers) => {
-  let arr1 = [];
-  let arr2 = [];
-  for (let number of numbers) {
-    if (arr1.length === 0) {
-      arr1.push(number);
-    } else if (arr1[0] === number) {
-      arr1.push(number);
-    } else {
-      arr2.push(number);
+  for (let i in numbers) {
+    if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+      return numbers[i];
     }
   }
-  return (arr1.length === 1 ? Number(arr1) : Number(arr2));
 };
 
 console.log(stray([17, 17, 3, 17, 17, 17, 17])); // 3
