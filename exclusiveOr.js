4eab93c98152ca1ff76149107f28b9a64752843f
Neale Taylor <nealeptaylor@gmail.com>
2022-09-16 14:49:06 -0400

    add tests

--- exclusiveOr.js
+++ exclusiveOr.js
@@ -9,5 +9,9 @@
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 
 const xor = (a, b) => {
-  
+
 };
+
+console.log(xor(false, false)); // false;
+console.log(xor(false, true)); // true;
+console.log(xor(true, true)); // false;
