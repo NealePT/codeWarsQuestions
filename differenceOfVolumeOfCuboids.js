57e7c9172953d41225f8495f1efe380359fa5abc
Neale Taylor <nealeptaylor@gmail.com>
2022-09-19 13:26:59 -0400

    implement solution

--- differenceOfVolumeOfCuboids.js
+++ differenceOfVolumeOfCuboids.js
@@ -5,7 +5,7 @@
 // Your function will be tested with pre-made examples as well as random ones.
 
 const findDifference = (a, b) => {
-  
+  return Math.abs(a[0] * a[1] * a[2] - b[0] * b[1] * b[2]);
 };
 
 console.log(findDifference([3, 2, 5], [1, 4, 4])); // 14
