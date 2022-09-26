90443dfcf11f06e339dccd0078380fe442eb31f7
Neale Taylor <nealeptaylor@gmail.com>
2022-09-26 12:47:50 -0400

    add tests

--- theRealFloor.js
+++ theRealFloor.js
@@ -9,3 +9,9 @@
 const getRealFloor = (n) => {
 
 };
+
+console.log(getRealFloor(1)); // 0
+console.log(getRealFloor(0)); // 0
+console.log(getRealFloor(5)); // 4
+console.log(getRealFloor(15)); // 13
+console.log(getRealFloor(-3)); // -3
