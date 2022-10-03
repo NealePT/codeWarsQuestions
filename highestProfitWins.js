820848b929e972d99c159de285c0df70e0905921
Neale Taylor <nealeptaylor@gmail.com>
2022-10-03 12:55:53 -0400

    implement solution

--- highestProfitWins.js
+++ highestProfitWins.js
@@ -6,7 +6,17 @@
 // [1]         --> [1,1]
 
 const minMax = (arr) => {
-
+  let min;
+  let max;
+  for (let i = 0; i < arr.length; i++) {
+    if (!min || arr[i] < min) {
+      min = arr[i];
+    }
+    if (!max || arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return [min, max];
 };
 
 console.log(minMax([1,2,3,4,5])); // [1,5]
