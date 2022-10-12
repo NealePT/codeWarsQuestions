e2f6aa2aa0cef2507deeacfb9d04211f74e1aed6
Neale Taylor <nealeptaylor@gmail.com>
2022-10-12 11:45:47 -0400

    implement solution

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -14,7 +14,12 @@
 // 10 * 5 = 50
 
 const multiTable = (number) => {
-
+  let result = [];
+  for (let i = 1; i < 10; i++) {
+    result.push(`${i} * ${number} = ${i * number}\n`);
+  }
+  result.push(`${10} * ${number} = ${10 * number}`);
+  return result.join("");
 };
 
 console.log(multiTable(5)); // '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50');
