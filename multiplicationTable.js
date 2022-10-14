d0cd2ae283534b790fd9d833544bae02c049743c
Neale Taylor <nealeptaylor@gmail.com>
2022-10-14 10:46:24 -0400

    spacing

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -13,7 +13,6 @@
 // 9 * 5 = 45
 // 10 * 5 = 50
 
-
 const multiTable = (number) => {
   let result = [];
   for (let i = 1; i < 10; i++) {
@@ -22,6 +21,5 @@ const multiTable = (number) => {
   result.push(`${10} * ${number} = ${10 * number}`);
   return result.join("");
 };
-
 
 console.log(multiTable(5)); // '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50');
