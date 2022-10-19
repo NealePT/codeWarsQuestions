ca777ef98e62483f7841f979c2f79fa3069af847
Neale Taylor <nealeptaylor@gmail.com>
2022-10-19 12:45:04 -0400

    add tests

--- twoSum.js
+++ twoSum.js
@@ -9,3 +9,8 @@
 const twoSum = (numbers, target) => {
 
 };
+
+console.log(twoSum([1,2,3], 4)); // [0,2]
+console.log(twoSum([1234,5678,9012], 14690)); // [1,2]
+console.log(twoSum([2,2,3], 4)); // [0,1]
+console.log(twoSum([2,3,1], 4)); // [1,2]
