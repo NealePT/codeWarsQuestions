5666c1a6826a7ff85b3001ee92a6245132d406ca
Neale Taylor <nealeptaylor@gmail.com>
2022-10-28 13:19:47 -0400

    implement solution

--- flattenAndSortArray.js
+++ flattenAndSortArray.js
@@ -11,7 +11,13 @@
 // Please, keep in mind, that JavaScript is by default sorting objects alphabetically. For more information, please consult:
 
 const flattenAndSort = (array) => {
-
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      result.push(array[i][j]);
+    }
+  }
+  return result.sort((a,b) => (a - b));
 };
 
 console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]])); // [1,2,3,4,5,6,7,8,9]
