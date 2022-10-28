97a88a451d699d4d04b1677082b9d7426e568812
Neale Taylor <nealeptaylor@gmail.com>
2022-10-28 13:12:15 -0400

    add tests

--- flattenAndSortArray.js
+++ flattenAndSortArray.js
@@ -13,3 +13,8 @@
 const flattenAndSort = (array) => {
 
 };
+
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]])); // [1,2,3,4,5,6,7,8,9]
+console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]])); // [1, 2, 3, 4, 5, 6, 100]
+console.log(flattenAndSort([[], [1]])); // [1]
+console.log(flattenAndSort([[], []])); // []
