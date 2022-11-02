71882ffb783fce60bb05fb72188084e57f4a2b52
Neale Taylor <nealeptaylor@gmail.com>
2022-11-02 12:28:31 -0400

    implement solution

--- smallEnough.js
+++ smallEnough.js
@@ -3,10 +3,15 @@
 // You can assume all values in the array are numbers.
 
 const smallEnough = (array, limit) => {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > limit) {
+      return false;
+    }
+  }
+  return true;
 };
 
 console.log(smallEnough([66, 101], 200)); // true
-console.log([78, 117, 110, 99, 104, 117, 107, 115], 100); // false
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); // false
 console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); // true
 console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); // true
