282c1439cc76c3f88fa809e9e3a35ffb845e552c
Neale Taylor <nealeptaylor@gmail.com>
2022-11-10 15:31:57 -0500

    add tests

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -9,3 +9,7 @@
 const uniqueInOrder = (s) => {
 
 };
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB')); // ['A', 'B', 'C', 'D', 'A', 'B']
+console.log(uniqueInOrder('ABBCcAD')); // ['A', 'B', 'C', 'c', 'A', 'D']
+console.log(uniqueInOrder([1,2,2,3,3])); // [1,2,3]
