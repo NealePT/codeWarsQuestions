c526e2bc004864f714e079158e97ec11a275a410
Neale Taylor <nealeptaylor@gmail.com>
2022-11-11 11:17:13 -0500

    add tests

--- checkTheExam.js
+++ checkTheExam.js
@@ -14,3 +14,8 @@
 const checkExam = (arr) => {
 
 };
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])); // 6
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""])); // 7
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"])); // 16
+console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"])); // 0
