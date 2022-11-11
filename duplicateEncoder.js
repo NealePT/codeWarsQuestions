ecb50829921d45babcbfc64f9d7239455ad97c3f
Neale Taylor <nealeptaylor@gmail.com>
2022-11-11 10:33:14 -0500

    add tests

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -4,10 +4,15 @@
 // "din"      =>  "((("
 // "recede"   =>  "()()()"
 // "Success"  =>  ")())())"
-// "(( @"     =>  "))((" 
+// "(( @"     =>  "))(("
 // Notes
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
 const duplicateEncoder = (word) => {
 
 };
+
+console.log(duplicateEncoder("din")); // "((("
+console.log(duplicateEncoder("recede")); // "()()()"
+console.log(duplicateEncoder("Success")); // ")())())"
+console.log(duplicateEncoder("(( @")); // "))(("
