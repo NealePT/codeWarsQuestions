e17c7e073d9750aabf3e1f29430f57268b2d3d28
Neale Taylor <nealeptaylor@gmail.com>
2022-11-11 13:43:29 -0500

    implement solution

--- sumOfMinimums.js
+++ sumOfMinimums.js
@@ -11,8 +11,18 @@
 // Note: You will always be given a non-empty list containing positive values.
 
 const sumOfMinimums = (arr) => {
-
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let min = 0;
+    for (let j = 0; j < arr[i].length; j++) {
+      if (min === 0 || arr[i][j] < min) {
+        min = arr[i][j];
+      }
+    }
+    result += min;
+  }
+  return result;
 };
 
-console.log([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]); // 9
-console.log([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]); // 76
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])); // 9
+console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]])); // 76
