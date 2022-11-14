55103133ee271223518e752a511d63cc730a8e89
Neale Taylor <nealeptaylor@gmail.com>
2022-11-14 15:26:33 -0500

    add tests

--- countingDuplicates.js
+++ countingDuplicates.js
@@ -12,3 +12,11 @@
 const duplicateCount = (string) => {
 
 };
+
+console.log(duplicateCount("abcde")); // 0
+console.log(duplicateCount("aabbcde")); // 2
+console.log(duplicateCount("aabBcde")); // 2
+console.log(duplicateCount("indivisibility")); // 1
+console.log(duplicateCount("Indivisibilities")); // 2
+console.log(duplicateCount("aA11")); // 2
+console.log(duplicateCount("ABBA")); // 2
