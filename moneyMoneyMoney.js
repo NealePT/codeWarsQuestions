072709c1533b6cc23a9e10bd3126311773c0d645
Neale Taylor <nealeptaylor@gmail.com>
2022-11-17 12:50:49 -0500

    add tests

--- moneyMoneyMoney.js
+++ moneyMoneyMoney.js
@@ -27,3 +27,7 @@
 const calculateYears = (principal, interest, tax, desired) => {
 
 };
+
+console.log(calculateYears(1000, 0.05, 0.18, 1100)); // 3
+console.log(calculateYears(1000,0.01625,0.18,1200)); // 3
+console.log(calculateYears(1000,0.05,0.18,1000)); // 3
