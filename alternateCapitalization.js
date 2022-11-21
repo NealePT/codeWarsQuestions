85afc6a6efe4c3f083ee9fb60dd4c613e70641e8
Neale Taylor <nealeptaylor@gmail.com>
2022-11-21 14:48:52 -0500

    implement solution

--- alternateCapitalization.js
+++ alternateCapitalization.js
@@ -5,7 +5,18 @@
 // The input will be a lowercase string with no spaces.
 
 const capitalize = (string) => {
-
+  let resultA = [];
+  let resultB = [];
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      resultA.push(string[i].toUpperCase());
+      resultB.push(string[i]);
+    } else {
+      resultA.push(string[i]);
+      resultB.push(string[i].toUpperCase());
+    }
+  }
+  return [resultA.join(""), resultB.join("")];
 };
 
 console.log(capitalize("abcdef")); // ['AbCdEf', 'aBcDeF']
