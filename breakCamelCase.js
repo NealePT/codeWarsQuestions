98bd5d7ec9a8dfa9abff7e21da9186859f70c58b
Neale Taylor <nealeptaylor@gmail.com>
2022-11-21 14:24:28 -0500

    implement solution

--- breakCamelCase.js
+++ breakCamelCase.js
@@ -6,7 +6,15 @@
 // ""             =>  ""
 
 const solution = (string) => {
-
+  let splitStr = string.split("");
+  let newStr = string.split("");
+  let capStr = string.toUpperCase().split("");
+  for (let i = splitStr.length - 1; i >= 0; i--) {
+    if (splitStr[i] === capStr[i]) {
+      newStr.splice(i, 0, ' ');
+    }
+  }
+  return newStr.join("");
 };
 
 console.log(solution("camelTestCasing")); // "camel Test Casing"
