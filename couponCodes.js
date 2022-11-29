8a9980c80e00c0414a52b439d9a0b4c108a5d2bd
Neale Taylor <nealeptaylor@gmail.com>
2022-11-29 16:13:24 -0500

    implement solution

--- couponCodes.js
+++ couponCodes.js
@@ -12,8 +12,10 @@
 // checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 
 const checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) => {
-
+  return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate);
 };
 
 console.log(checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")); // true
 console.log(checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")); // false
+console.log(checkCoupon("123", "123", "August 9, 2015", "July 9, 2015")); // false
+console.log(checkCoupon("123", "123", "July 9, 2015", "July 9, 2014")); // false
