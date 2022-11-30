4cd7585cb8a110881d35039135590592ff642fe5
Neale Taylor <nealeptaylor@gmail.com>
2022-11-30 15:31:44 -0500

    add tests

--- titleCase.js
+++ titleCase.js
@@ -16,3 +16,7 @@
 const titleCase = (title, minorWords) => {
 
 };
+
+console.log(titleCase('a clash of KINGS', 'a an the of')); // A Clash of Kings
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In')); // The Wind in the Willows
+console.log(titleCase('the quick brown fox')); // The Quick Brown Fox
