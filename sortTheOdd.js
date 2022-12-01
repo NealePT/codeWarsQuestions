988213707ec10f0f0b6eec86018197a670c3ecb5
Neale Taylor <nealeptaylor@gmail.com>
2022-12-01 16:08:21 -0500

    add starter code

---
+++ sortTheOdd.js
@@ -0,0 +1,5 @@
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+const sortArray = (array) => {
+
+};
