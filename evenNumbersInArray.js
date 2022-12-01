1847101a571c1bb4ee0ce598a84ef2618a5c9916
Neale Taylor <nealeptaylor@gmail.com>
2022-12-01 16:03:05 -0500

    implement solution

--- evenNumbersInArray.js
+++ evenNumbersInArray.js
@@ -1,7 +1,16 @@
 // Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
 
 const evenNumbers = (array, number) => {
-
+  let result = [];
+  array = array.reverse();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      result.push(array[i]);
+    }
+    if (result.length === number) {
+      return result.reverse();
+    }
+  }
 };
 
 console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); // [4, 6, 8]
