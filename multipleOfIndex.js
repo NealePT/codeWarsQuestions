502e10dbd9aefce0ed677cdaa190c77ad499554c
Neale Taylor <nealeptaylor@gmail.com>
2022-12-02 13:11:53 -0500

    implement solution

--- multipleOfIndex.js
+++ multipleOfIndex.js
@@ -1,9 +1,15 @@
 // Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 
-const mulipleOfIndex = (array) => {
-
+const multipleOfIndex = (array) => {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Number.isInteger(array[i] / i)) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 };
 
-console.log(mulipleOfIndex([22, -6, 32, 82, 9, 25])); // [-6, 32, 25]
-console.log(mulipleOfIndex([68, -1, 1, -7, 10, 10])); // [-1, 10]
-console.log(mulipleOfIndex([-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68])); // [-85, 72, 0, 68]
+console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])); // [-6, 32, 25]
+console.log(multipleOfIndex([68, -1, 1, -7, 10, 10])); // [-1, 10]
+console.log(multipleOfIndex([-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68])); // [-85, 72, 0, 68]
