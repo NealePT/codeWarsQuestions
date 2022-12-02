c985e8329dbc2bf7949cf86c768ee6bb59b4ba8c
Neale Taylor <nealeptaylor@gmail.com>
2022-12-02 13:08:59 -0500

    add tests

--- multipleOfIndex.js
+++ multipleOfIndex.js
@@ -1,5 +1,9 @@
-Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 
 const mulipleOfIndex = (array) => {
 
 };
+
+console.log(mulipleOfIndex([22, -6, 32, 82, 9, 25])); // [-6, 32, 25]
+console.log(mulipleOfIndex([68, -1, 1, -7, 10, 10])); // [-1, 10]
+console.log(mulipleOfIndex([-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68])); // [-85, 72, 0, 68]
