871799525ba73bf32bff870e10f4be29a57cad26
Neale Taylor <nealeptaylor@gmail.com>
2022-12-08 15:34:19 -0500

    implement solution

--- alphabetWar.js
+++ alphabetWar.js
@@ -26,7 +26,34 @@
 // alphabetWar("wwwwwwz");  //=> Left side wins!
 
 const alphabetWar = (fight) => {
-
+  let leftSide = 0;
+  let rightSide = 0;
+  for (let i = 0; i < fight.length; i++) {
+    if (fight[i] === "w") {
+      leftSide = leftSide + 4;
+    } else if (fight[i] === "p") {
+      leftSide = leftSide + 3;
+    } else if (fight[i] === "b") {
+      leftSide = leftSide + 2;
+    } else if (fight[i] === "s") {
+      leftSide = leftSide + 1;
+    } else if (fight[i] === "m") {
+      rightSide = rightSide + 4;
+    } else if (fight[i] === "q") {
+      rightSide = rightSide + 3;
+    } else if (fight[i] === "d") {
+      rightSide = rightSide + 2;
+    } else if (fight[i] === "z") {
+      rightSide = rightSide + 1;
+    }
+  }
+  if (rightSide > leftSide) {
+    return "Right side wins!";
+  } else if (leftSide > rightSide) {
+    return "Left side wins!";
+  } else {
+    return "Let's fight again!";
+  }
 };
 
 console.log(alphabetWar("z")); // Right side wins!
