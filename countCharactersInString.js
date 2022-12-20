ce98b92664459306e261f76e3ce37a1607157951
Neale Taylor <nealeptaylor@gmail.com>
2022-12-20 10:33:13 -0500

    implement solution

--- countCharactersInString.js
+++ countCharactersInString.js
@@ -3,7 +3,16 @@
 // What if the string is empty? Then the result should be empty object literal, {}.
 
 const count = (string) => {
-
+  let result = {};
+  let split = string.split("");
+  for (let i = 0; i < split.length; i++) {
+    if (!Object.keys(result).includes(split[i])) {
+      result[split[i]] = 1;
+    } else if (Object.keys(result).includes(split[i])) {
+      result[split[i]] = result[split[i]] + 1;
+    }
+  }
+  return result;
 };
 
 console.log(count("abc")); // { a: 1, b: 1, c: 1 })
