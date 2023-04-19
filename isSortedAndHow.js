c3c8db0f342eed6c0f614b76b21571f933df49e3
Neale Taylor <nealeptaylor@gmail.com>
2023-04-19 12:43:34 -0400

    add testing

--- isSortedAndHow.js
+++ isSortedAndHow.js
@@ -8,3 +8,7 @@
 const isSortedAndHow = (arr) => {
 
 };
+
+console.log(isSortedAndHow([1, 2])); // yes, ascending
+console.log(isSortedAndHow([15, 7, 3, -8])); // yes, descending
+console.log(isSortedAndHow([4, 2, 30])); // no
