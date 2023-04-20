fac01bdf459ebb8e290f322cdad5d0a7816fa94a
Neale Taylor <nealeptaylor@gmail.com>
2023-04-20 11:26:46 -0400

    clean up

--- isSortedAndHow.js
+++ isSortedAndHow.js
@@ -3,7 +3,6 @@
 // "yes, ascending" - if the numbers in the array are sorted in an ascending order
 // "yes, descending" - if the numbers in the array are sorted in a descending order
 // "no" - otherwise
-// You can assume the array will always be valid, and there will always be one correct answer.
 
 const isSortedAndHow = (arr) => {
   let str = arr.join("");
